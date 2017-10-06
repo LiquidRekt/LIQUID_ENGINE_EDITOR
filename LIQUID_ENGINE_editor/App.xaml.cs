baab575ca8fe71d69bcedad0fdc11eb10671d3ed
LiquidRekt <dung6ctd@gmail.com>
2017-10-06 17:32:25 +0700

    Add files via upload

---
+++ LIQUID_ENGINE_editor/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace LIQUID_ENGINE_editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ LIQUID_ENGINE_editor/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LIQUID_ENGINE_editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void coming_Soon()
+        {
+            MessageBox.Show("Coming Soon!");
+        }
+
+        private void create_Project(object sender, RoutedEventArgs e)
+        {
+            CreatingProject createProject = new CreatingProject();
+            createProject.Show();
+            this.Close();
+        }
+        private void open_Project(object sender, RoutedEventArgs e)
+        {
+            OpenProject openProject = new OpenProject();
+            openProject.Show();
+            this.Close();
+        }
+        private void download_Assets(object sender, RoutedEventArgs e)
+        {
+            DownloadAssets downloadAssets = new DownloadAssets();
+            downloadAssets.Show();
+            this.Close();
+        }
+    }
+   
+}
